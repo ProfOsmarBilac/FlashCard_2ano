68a34e9b1b44384b6203f88441d8f849930e2d42
Osmar Crócoli <161791879+ProfOsmarBilac@users.noreply.github.com>
2024-10-25 23:44:23 -0300

    Flashcard finalizado

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de códigos que executa alguma tarefa.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que significa congruente?',
+    'Congruente é quando tem a mesma medida: sinônimo de igual.'
+)
+
+criaCartao(
+    'Linguagem',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é Hi (Rai).'
+)
+
+criaCartao(
+    'Física',
+    'O que é MRUV?',
+    'MRUV significa Movimento Retilíneo Uniformemente Variado.'
+)
